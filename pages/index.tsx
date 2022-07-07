d47a9f16ca3a7df5793f0555b63406a285936ac0
rjames <rjames@rjames-mbpr-us.lan>
2022-07-07 18:04:51 -0400

    quiz sample complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,115 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import questions from '../questions.json'
 
 const Home: NextPage = () => {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handlePrevious = () => {
+    const prevQues = currentQuestion - 1;
+    prevQues >= 0 && setCurrentQuestion(prevQues);
+  };
+
+  const handleNext = () => {
+    const nextQues = currentQuestion + 1;
+    nextQues < questions.length && setCurrentQuestion(nextQues);
+  }
+
+  const handleAnswerOption = (answer: String) => {
+    setSelectedOptions([
+      (selectedOptions[currentQuestion] = {answerByUser: answer}),
+    ]);
+    setSelectedOptions([...selectedOptions]);
+    console.log(selectedOptions)
+  }
+
+  const handleSubmitButton = () => {
+    let newScore = 0;
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].answerOptions.map(
+        (answer) =>
+          answer.isCorrect &&
+          answer.answer === selectedOptions[i]?.answerByUser &&
+          (newScore += 1)
+      );
+    }
+    setScore(newScore);
+    setShowScore(true);
+  }
+
+
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-[#1A1A1A]">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Quiz App</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex flex-col items-center justify-center w-screen px-5 h-screen bg-[#1A1A1A]">
+        
+        {showScore ? (
+          <h1 className="text-3xl font-semibold text-center text-white">
+            You scored {score} out of {questions.length}
+          </h1>
+          ) : (
+        <>
+        <div className="flex flex-col items-start w-full">
+          <h4 className="mt-10 text-xl text-white/60">Question {currentQuestion + 1} of {questions.length}</h4>
+          <div className="mt-4 text-2xl text-white">
+            {questions[currentQuestion].question}
+          </div>
         </div>
-      </main>
+
+        <div className="flex flex-col w-full">
+          {questions[currentQuestion].answerOptions.map((answer, index) => (
+            <div
+              key={index}
+              className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-white/5 border-white/10 rounded-xl"
+              onClick={(e) => handleAnswerOption(answer.answer)}
+            >
+              <input 
+                type="radio" 
+                name={answer.answer}
+                value={answer.answer}
+                onChange={(e) => handleAnswerOption(answer.answer)}
+                checked={answer.answer === selectedOptions[currentQuestion]?.answerByUser}
+                className="w-6 h-6 bg-black" />
+              <p className="ml-6 text-white">{answer.answer}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-between w-full mt-4 text-white">
+          <button 
+            onClick={handlePrevious} 
+            className="w-[49%] py-3 bg-indigo-600 rounded-lg"
+            >Previous
+          </button>
+          <button 
+            onClick={
+              currentQuestion + 1 === questions.length ? handleSubmitButton : handleNext
+            } 
+            className="w-[49%] py-3 bg-indigo-600 rounded-lg"
+            >{currentQuestion + 1 === questions.length ? 'Submit' : 'Next'}
+          </button>
+        </div>
+        </>
+          )};
+
+      </div>
+
+      
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+        <Link href="https://github.com/ryanjames1729"><a>Made by Ryan James</a></Link>
       </footer>
     </div>
   )
